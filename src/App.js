b3d6dbe147723cb7045c78f0229246672678bec8
Jesus Avila Miranda <jesusavila@my.unt.edu>
2024-02-27 14:05:16 -0600

    initial react app code setup

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://www.youtube.com/shorts/zQrq9B4KlvA"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Luis Roman
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Jesus Avila
+        </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
