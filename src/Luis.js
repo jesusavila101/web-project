8bda3a01c38aa54456117fc22f9c9b2b511c22fc
Jesus Avila Miranda <jesusavila@my.unt.edu>
2024-02-28 20:49:03 -0600

    new

---
+++ src/Luis.js
@@ -0,0 +1,13 @@
+// Luis.js
+import React from "react";
+
+const Luis = () => {
+  return (
+    <div>
+      <h2>Luis Page</h2>
+      <p>This is the content for Luis' page.</p>
+    </div>
+  );
+};
+
+export default Luis;
